845cbb1b049c06f3b7b1fac48bb1809fd218238f
ImananAkihc <wujianyu_happy@pku.edu.cn>
2025-08-29 04:07:06 +0800

    dockerfile

--- setup.sh
+++ setup.sh
@@ -1,11 +1,17 @@
-sudo apt install -y python3-numpy openjdk-8-jdk libtcmalloc-minimal4 libgoogle-perftools-dev
+sudo apt install -y python3-numpy openjdk-8-jdk libtcmalloc-minimal4 libgoogle-perftools-dev docker.io
+sudo systemctl enable --now docker
+sudo usermod -aG docker $USER
+
+docker load -i ubuntu_latest.tar
+docker build --no-cache -t be_container:v1 -f ./BE.Dockerfile .
+docker build --no-cache -t be_container:v1 -f ./LC.Dockerfile .
 
 cd /home/wjy
 git clone git@github.com:intel/intel-cmt-cat.git
 cd intel-cmt-cat
 make
 sudo cp ./pqos/pqos /usr/local/bin/
-echo "/home/wjy/SComet/intel-cmt-cat/lib" | sudo tee /etc/ld.so.conf.d/pqos.conf
+echo "/home/wjy/intel-cmt-cat/lib" | sudo tee /etc/ld.so.conf.d/pqos.conf
 sudo ldconfig
 
 cd /home/wjy/SComet/benchmarks/Tailbench/tailbench
